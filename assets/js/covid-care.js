db9d57611627c7c4263e1a26908667bc56d564e0
Saransh <saranshs@nookal.com>
2021-04-22 21:02:43 +0530

    Covid care facilities

---
+++ assets/js/covid-care.js
@@ -0,0 +1,28 @@
+jQuery(
+  (function () {
+    "use strict";
+
+    $.getJSON(
+      "https://content-sheets.googleapis.com/v4/spreadsheets/1ZyrYsowjk6PdC9N5yKBxMslI7FypoeIqDvlAYrqprL8/values/Covid%20Care%20Center!A3%3AJ?key=AIzaSyCAp-t_WKWx0sfpOM5_-5yeC9g-ZcrkWcI",
+      function (data) {
+        console.log(data);
+        if (
+          data != undefined &&
+          data.values != undefined &&
+          data.values.length != 0
+        ) {
+          let html = "";
+          $.each(data.values, function (i, v) {
+            let date = moment(v[9], "DD/MM/YYYY HH:mm:ss");
+            let now = moment();
+            let formattedDate = now.from(date, "days") + " ago";
+
+            html += `<tr><th>${v[0]}</th><td>${v[1]}</td><td>${v[2]}</td><td>${v[3]}</td><td>${v[4]}</td><td>${v[5]}</td><td>${v[6]}</td><td>${v[7]}</td><td>${formattedDate}</td></tr>`;
+          });
+
+          $("#covid-table").html(html);
+        }
+      }
+    );
+  })()
+);
---
+++ covid-care.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="Saransh Seth">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <!-- Fav Icon  -->
+  <link rel="shortcut icon" href="images/favicon.svg">
+  <!-- Site Title  -->
+  <title>COVID-19 Madad | Initiative to help COVID affected</title>
+  <!-- Bundle and Base CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf_viewer.min.css" />
+  <link rel="stylesheet" href="assets/css/bundle.css?ver=112">
+  <link rel="stylesheet" href="assets/css/styles.css?ver=112">
+  <link rel="stylesheet" href="assets/css/remedesivir.css?ver=112">
+</head>
+
+<body class="nk-body">
+  <div class="top-bar">COVID 19 Helpline <a href="tel:+917926403333">079 26403333</a> | <a href="tel:+917926409999">079
+      26409999</a></div>
+  <?php include('header-2.html'); ?>
+  <div class="nk-banner">
+    <div class="container-lg">
+      <div class="row">
+        <h2 class="col">COVID Care Facilities</h2>
+      </div>
+
+      <table class="table mt-3 table-striped table-responsive-md table-bordered">
+
+        <tr>
+          <th>#</th>
+          <th>City</th>
+          <th>Organisation</th>
+          <th>Address</th>
+          <th>Person</th>
+          <th>Contact</th>
+          <th>Remarks</th>
+          <th>Status</th>
+          <th>Verified On</th>
+        </tr>
+
+        <tbody id="covid-table">
+
+        </tbody>
+      </table>
+
+    </div>
+  </div>
+
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+  <script src="https://kit.fontawesome.com/852453daca.js" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.worker.min.js"></script>
+  <script src="assets/js/bundle.js?ver=112"></script>
+  <script src="assets/js/scripts.js?ver=112"></script>
+  <script src="assets/js/covid-care.js?ver=112"></script>
+
+  <?php include('footer.html'); ?>
+</body>
+
+</html>
