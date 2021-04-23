f3fbf89e1bf42038447f18f48f64503cf992cf57
Saransh <saranshs@nookal.com>
2021-04-23 17:28:14 +0530

    admission criteria for covid patients by ahna

---
+++ admission.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="Softnio">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <!-- Fav Icon  -->
+  <link rel="shortcut icon" href="images/favicon.svg">
+  <!-- Site Title  -->
+  <title>COVID-19 Madad | Initiative to help COVID affected</title>
+  <!-- Bundle and Base CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf_viewer.min.css" />
+  <link rel="stylesheet" href="assets/css/bundle.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="assets/css/admission.css">
+</head>
+
+<body class="nk-body">
+  <div class="top-bar">COVID 19 Helpline <a href="tel:+917926403333">079 26403333</a> | <a href="tel:+917926409999">079
+      26409999</a></div>
+  <?php include('header-2.html'); ?>
+  <div class="nk-banner">
+    <div class="container-lg">
+      <div class="row">
+        <h3 class="col">Admission Criteria for COVID 19 Patients <span class="badge bg-secondary">PRESS
+            RELEASE - 22nd
+            November 2020(AHNA)</span></h3>
+      </div>
+      <div class="row mt-3 p-3">
+        <p class="">The city of Ahmedabad is witnessing an increase in the number of Covid cases since last few days and
+          the
+          bed availability has become an issue.</p>
+        <p>
+          The Covid experts of Ahmedabad Hospitals & Nursing Homes Association had an emergency meeting to finalise the
+          criteria for admitting Covid patients in the designated Covid care Facilities.</p>
+        <p>
+          It is well known that in Covid 19 infection, nearly <b>85% to 90% patients</b> will have mild to moderate
+          disease
+          and will recover on their own. The concern is only for the remaining 10 to 15% of patients who may require
+          hospitalisation.</p>
+        <p>
+          It is important to note here that children and young ones are at very low risk for complications and hence
+          there are very high chances that they will improve on their own and will not require hospitalisation.</p>
+
+        </p>
+      </div>
+      <div class="row mt-3 p-3">
+        <dl>
+          <dt>Admission Criteria 1:</dt>
+          <dd>Persons at risk of severe disease- includes<br />• 1(a): People above the age of 60<br />
+            • 1(b): Persons with Co-morbidities or associated medical conditions (HT, DM, IHD,
+            cardiomyopathies, post-transplant patient, patients with malignancy, Chronic kidney disease,
+            obesity, chronic liver disease, HIV infection, Chronic neurologic condition, COPD, etc.)<br />
+            • 1(c): Patients on medications such as immunosuppressive therapy, patients on steroids</dd>
+          <dt>Admission Criteria 2:</dt>
+          <dd>Any patient with persistent fever more than 101 F for more than three days</dd>
+          <dt>Admission Criteria 3:</dt>
+          <dd>Any patient with oxygen saturation (SPO2) less than 94 % on room air, irrespective of age</dd>
+          <dt>Admission Criteria 4:</dt>
+          <dd>Any patient with significant breathing discomfort and increased respiratory rate irrespective of
+            age</dd>
+          <dt>Admission Criteria 5:</dt>
+          <dd>Any patient with features suggestive of non-lung complications e.g. drowsiness, chest pain etc.
+            irrespective of age</dd>
+        </dl>
+
+      </div>
+
+      <div class="row mt-3 p-3">
+        <p>Furthermore, it is clarified that the final decision regarding admission is of the treating doctor and these
+          will remain broad guidelines as it is difficult to list down all the conditions for which admission may be
+          required.</p>
+        <p>Patients with suspected or confirmed moderate COVID-19 (pneumonia) be isolated to contain virus
+          transmission. Patients with moderate illness may not require emergency interventions or
+          hospitalization; however, isolation is necessary for all suspect or confirmed cases.</p>
+        <p>The location of isolation will depend on the established COVID-19 care pathway and can be done at a
+          health facility, community facility or at home. The decision of location should be made on a case-bycase basis
+          and will depend on the clinical presentation, requirement for supportive care, potential risk
+          factors for severe disease, and conditions at home, including the presence of vulnerable persons in the
+          household.
+        </p>
+        <p>
+          For patients at high risk for deterioration, isolation in hospital is preferred.
+        </p>
+        <p>
+          The following are the common signs & symptoms which are seen in patients which may require
+          hospitalization:<br /><span class="">
+            &nbsp;&nbsp;&nbsp;1. Persistent fever >99.5 F<br />
+            &nbsp;&nbsp;&nbsp;2. Persistent severe cough<br />
+            &nbsp;&nbsp;&nbsp;3. Breathlessness<br />
+            &nbsp;&nbsp;&nbsp;4. SPo2
+            < 94%<br />
+            &nbsp;&nbsp;&nbsp;5. Persons who cannot self-isolate<br />
+          </span>
+        </p>
+        <p>
+          AHNA has also made standard protocols for treatment of patients with Injectable anti virals at home or
+          in a day care setting and the same has been made available to the treating doctors.
+        </p>
+        <h3><b>
+            The above details are being informed to the general public so that an informed decision can be taken.
+          </b></h3>
+      </div>
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.min.js"></script>
+    <script src="https://kit.fontawesome.com/852453daca.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="assets/js/patient-bed.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.worker.min.js"></script>
+    <script src="assets/js/bundle.js"></script>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
