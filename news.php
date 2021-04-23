2567ed0230e661f18eeb05e8e68bad9ce96de126
Saransh <saranshs@nookal.com>
2021-04-23 19:05:10 +0530

    Latest news

--- news.php
+++ news.php
@@ -24,19 +24,13 @@
   <div class="nk-banner">
     <div class="container-lg">
       <?php 
-        $all_files = glob("/news/*.*");
-        for ($i=0; $i<count($all_files); $i++)
-          {
-            $image_name = $all_files[$i];
-            $supported_format = array('gif','jpg','jpeg','png');
-            $ext = strtolower(pathinfo($image_name, PATHINFO_EXTENSION));
-            if (in_array($ext, $supported_format))
-                {
-                  echo '<img src="'.$image_name .'" alt="'.$image_name.'" />'."<br /><br />";
-                } else {
-                    continue;
-                }
-          }
+        $directory = "news";
+        $images = glob($directory . "/*.{jpg,png,bmp}", GLOB_BRACE);
+        echo count($images);
+        foreach($images as $image)
+        { ?>
+      <img width="300" src="/<?php echo $image; ?>" />
+      <?php }
       ?>
     </div>
 
