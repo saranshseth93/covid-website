ffdde3fcd087badc3a4b90afaf7106d98cf194e6
Saransh <saranshs@nookal.com>
2021-04-24 19:11:21 +0530

    Added sorting for tables

--- assets/js/remedesivir.js
+++ assets/js/remedesivir.js
@@ -1,6 +1,13 @@
 jQuery(
   (function () {
     "use strict";
-
+    $(".table").DataTable({
+      paging: false,
+      info: false,
+      aaSorting: [
+        [4, "asc"],
+        [6, "desc"]
+      ]
+    });
   })()
 );
--- remedesivir.php
+++ remedesivir.php
@@ -47,173 +47,219 @@
             class="alert-link">remdesivir.tossilamc@gmail.com</a> with above mentioned 4 documents from hospital ID.
           If any documents are missing, application can be rejected.
           Successful application will get appointment for collecting.
+          <hr>
+          There is a very big shortage state-wide for Remedesivir. It is highly recommended to be patient. These numbers
+          are verified, but it might be possible that it might not be working when you call or might be busy. <b>We are
+            trying our best to serve the most updated resources.</b>
         </div>
       </div>
 
       <table class="table table-striped table-responsive table-bordered">
-        <tr>
-          <th>#</th>
-          <th>City</th>
-          <th>Organisation</th>
-          <th>Contact</th>
-          <th>Status</th>
-          <th>Remarks</th>
-          <th>Verified On</th>
-        </tr>
-        <tr>
-          <th>1</th>
-          <td>Ahmedabad</td>
-          <td>SVP Hospital, AMC, Ahmedabad (Hiren Vora)</td>
-          <td><a href="tel:+919825175560">9825175560</a></td>
-          <td>Reach via Private Hospital where patient is admitted</td>
-          <td>Check Notes Above</td>
-          <td>19-04-2021</td>
-        </tr>
-        <tr class="table-secondary">
-          <th>2</th>
-          <td>Ahmedabad</td>
-          <td>AMC, Ahmedabad</td>
-          <td><a href="tel:+919327535626">9327535626</a></td>
-          <td>Unreachable</td>
-          <td>8pm-8am</td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>3</th>
-          <td>Ahmedabad</td>
-          <td>Zydus Hospial, Ahmedabad</td>
-          <td>At venue only</td>
-          <td>Starting from 20/04/2021</td>
-          <td>8:30am-5:30pm, Aadhar, Prescription, One injection per person</td>
-          <td>19/04/2021</td>
-        </tr>
-        <tr>
-          <th>4</th>
-          <td>Ahmedabad</td>
-          <td>Medical Supritendent, Civil Hospital, Sola, Ahmedabad</td>
-          <td>At venue only</td>
-          <td>Reach via Private Hospital where patient is admitted</td>
-          <td>Aadhar, RTPCR test, Application Form, Prescription</td>
-          <td></td>
-        </tr>
-        <tr class="table-danger">
-          <th>5</th>
-          <td>Ahmedabad</td>
-          <td>AEON Healthcare, 405, Aeon Complex, opp. Navkar Institute, nr. Vijay Cross Road, Navrangpura, Ahmedabad
-            (Kunal Shah)</td>
-          <td><a href="tel:+919898214844">9898214844</a></td>
-          <td>Out of stock</td>
-          <td>Call before visiting. Aadhar card of patient, MD certificate of Prescription and corona positive report of
-            laboratory</td>
-          <td>17/04/2021</td>
-        </tr>
-        <tr class="table-warning">
-          <th>6</th>
-          <td>Ahmedabad</td>
-          <td>GCS Hospital</td>
-          <td><a href="tel:+919727769500">9727769500</a><br /><a href="tel:+917966048131">079-66048131</a></td>
-          <td>Stopped</td>
-          <td>8am-7pm</td>
-          <td></td>
-        </tr>
-        <tr class="table-danger">
-          <th>7</th>
-          <td>Ahmedabad</td>
-          <td>Rajkot District Chemist Council Mayursinh Jadeja & Animesh M Desai</td>
-          <td><a href="tel:+919426928345">9426928345</a><br /><a href="tel:+919484634463">9484634463</a><br /><a
-              href="tel:+919033430943">9033430943</a></td>
-          <td>Out of stock</td>
-          <td>Rs 1700 only</td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>8</th>
-          <td>Amreli</td>
-          <td>Om Sanjivani Medical, Rohit Rayani, Manishbhai dobariya Amreli</td>
-          <td><a href="tel:+919879277728">9879277728</a><br /><a href="tel:+919427455526">9427455526</a><br /><a
-              href="tel:+919824177655">9824177655</a></td>
-          <td>Available</td>
-          <td>Rs 1600 only, Aadhar card of patient, MD certificate of Prescription and corona positive report</td>
-          <td></td>
-        </tr>
-        <tr class="table-danger">
-          <th>9</th>
-          <td>Gujarat</td>
-          <td>CIPLA</td>
-          <td><a href="tel:+918657311088">8657311088</a><br /><a
-              href="mailto:info.availaibility@cipla.com">info.availaibility@cipla.com</a></td>
-          <td>Out of stock</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr class="table-danger">
-          <th>10</th>
-          <td>Gujarat</td>
-          <td>Hetero</td>
-          <td><a href="tel:04040473535">040-40473535</a><br /><a href="tel:18001034696">18001034696</a><br /><a
-              href="mailto:covifor@heterohealthcare.com">covifor@heterohealthcare.com</a></td>
-          <td>Out of stock</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr class="table-danger">
-          <th>11</th>
-          <td>Gujarat</td>
-          <td>Jubiliant</td>
-          <td><a href="tel:+919819857718">9819857718</a><br /><a
-              href="mailto:vikas.bansi@jubi.com">vikas.bansi@jubi.com</a><br /><a
-              href="tel:02262328244">022-62328244</a><br /><a href="mailto:jubi-R@jubl.com">jubi-R@jubl.com</a>
-          </td>
-          <td>Out of stock</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr class="table-danger">
-          <th>12</th>
-          <td>Gujarat</td>
-          <td>Mylan</td>
-          <td><a href="tel:+917829980066">7829980066</a>
-          </td>
-          <td>Out of stock</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>13</th>
-          <td>Gujarat</td>
-          <td>Taj Pharma</td>
-          <td>Direct Line:<a href="tel:+918448444095">+918448444095</a><br />Toll Free:<a
-              href="tel:1800222434">1-800-222-434</a><br />Direct Contact:<a
-              href="tel:+919833895800">9833895800</a><br /><a href="mailto:info@tajpharma.com">info@tajpharma.com</a>
-          </td>
-          <td>Available</td>
-          <td>Rs 35,000 only, Payment via account Courier via Mumbai</td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>14</th>
-          <td>India</td>
-          <td>Hetero Healthcare for COVIFOR Helpline number</td>
-          <td>Toll Free:<a href="tel:18001034696">18001034696</a>
-          </td>
-          <td>At Present in Waiting List</td>
-          <td><b><u>Process:</u></b><br />Call on the toll free number and register yourself(Registration is
-            compulsory). Then
-            you will receive a link on your phone after registration. Fill in all the details and <b>before submitting
-              take a screenshot of the details filled in. Without this screenshot injection won't be given.</b>After
-            that send the below things to whatsapp number: <a href="tel:+919320985814">9320985814</a><br />1) Screenshot
-            Taken<br />2)Original Prescription<br />3)Adhaar Card Photo<br />4)Covid +ve report.</td>
-          <td>21/04/2021</td>
-        </tr>
-        <!-- <tr>
-          <th>15</th>
-          <td>Rajkot</td>
-          <td>Rajkot Remdesivir Helpline</td>
-          <td>Send your case number and report details on Whatsapp</td>
-          <td>Available</td>
-          <td></td>
-          <td>21/04/2021</td>
-        </tr> -->
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>City</th>
+            <th>Organisation</th>
+            <th>Contact</th>
+            <th>Status</th>
+            <th>Remarks</th>
+            <th>Verified On</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Ahmedabad</td>
+            <td>SVP Hospital, AMC, Ahmedabad (Hiren Vora)</td>
+            <td><a href="tel:+919825175560">9825175560</a></td>
+            <td>Reach via Private Hospital where patient is admitted</td>
+            <td>Check Notes Above</td>
+            <td>19-04-2021</td>
+          </tr>
+          <tr class="table-warning">
+            <td>2</td>
+            <td>Ahmedabad</td>
+            <td>AMC, Ahmedabad</td>
+            <td><a href="tel:+919327535626">9327535626</a></td>
+            <td>Unreachable</td>
+            <td>8pm-8am</td>
+            <td></td>
+          </tr>
+          <tr class="table-warning">
+            <td>3</td>
+            <td>Ahmedabad</td>
+            <td>Zydus Hospial, Ahmedabad</td>
+            <td>At venue only</td>
+            <td>Stopped</td>
+            <td>8:30am-5:30pm, Aadhar, Prescription, One injection per person</td>
+            <td>24/04/2021</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Ahmedabad</td>
+            <td>Medical Supritendent, Civil Hospital, Sola, Ahmedabad</td>
+            <td>At venue only</td>
+            <td>Reach via Private Hospital where patient is admitted</td>
+            <td>Aadhar, RTPCR test, Application Form, Prescription</td>
+            <td></td>
+          </tr>
+          <tr class="table-danger">
+            <td>5</td>
+            <td>Ahmedabad</td>
+            <td>AEON Healthcare, 405, Aeon Complex, opp. Navkar Institute, nr. Vijay Cross Road, Navrangpura, Ahmedabad
+              (Kunal Shah)</td>
+            <td><a href="tel:+919898214844">9898214844</a></td>
+            <td>Out of stock</td>
+            <td>Call before visiting. Aadhar card of patient, MD certificate of Prescription and corona positive report
+              of
+              laboratory</td>
+            <td>17/04/2021</td>
+          </tr>
+          <tr class="table-warning">
+            <td>6</td>
+            <td>Ahmedabad</td>
+            <td>GCS Hospital</td>
+            <td><a href="tel:+919727769500">9727769500</a><br /><a href="tel:+917966048131">079-66048131</a></td>
+            <td>Stopped</td>
+            <td>8am-7pm</td>
+            <td></td>
+          </tr>
+          <tr class="table-danger">
+            <td>7</td>
+            <td>Ahmedabad</td>
+            <td>Rajkot District Chemist Council Mayursinh Jadeja & Animesh M Desai</td>
+            <td><a href="tel:+919426928345">9426928345</a><br /><a href="tel:+919484634463">9484634463</a><br /><a
+                href="tel:+919033430943">9033430943</a></td>
+            <td>Out of stock</td>
+            <td>Rs 1700 only</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>Amreli</td>
+            <td>Om Sanjivani Medical, Rohit Rayani, Manishbhai dobariya Amreli</td>
+            <td><a href="tel:+919879277728">9879277728</a><br /><a href="tel:+919427455526">9427455526</a><br /><a
+                href="tel:+919824177655">9824177655</a></td>
+            <td>Unreachable</td>
+            <td>Rs 1600 only, Aadhar card of patient, MD certificate of Prescription and corona positive report</td>
+            <td>24/06/2021</td>
+          </tr>
+          <tr class="table-danger">
+            <td>9</td>
+            <td>Gujarat</td>
+            <td>CIPLA</td>
+            <td><a href="tel:+918657311088">8657311088</a><br /><a
+                href="mailto:info.availaibility@cipla.com">info.availaibility@cipla.com</a></td>
+            <td>Out of stock</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr class="table-danger">
+            <td>10</td>
+            <td>Gujarat</td>
+            <td>Hetero</td>
+            <td><a href="tel:04040473535">040-40473535</a><br /><a href="tel:18001034696">18001034696</a><br /><a
+                href="mailto:covifor@heterohealthcare.com">covifor@heterohealthcare.com</a></td>
+            <td>Out of stock</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr class="table-danger">
+            <td>11</td>
+            <td>Gujarat</td>
+            <td>Jubiliant</td>
+            <td><a href="tel:+919819857718">9819857718</a><br /><a
+                href="mailto:vikas.bansi@jubi.com">vikas.bansi@jubi.com</a><br /><a
+                href="tel:02262328244">022-62328244</a><br /><a href="mailto:jubi-R@jubl.com">jubi-R@jubl.com</a>
+            </td>
+            <td>Out of stock</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr class="table-danger">
+            <td>12</td>
+            <td>Gujarat</td>
+            <td>Mylan</td>
+            <td><a href="tel:+917829980066">7829980066</a>
+            </td>
+            <td>Out of stock</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>13</td>
+            <td>Gujarat</td>
+            <td>Taj Pharma</td>
+            <td>Direct Line:<a href="tel:+918448444095">+918448444095</a><br />Toll Free:<a
+                href="tel:1800222434">1-800-222-434</a><br />Direct Contact:<a
+                href="tel:+919833895800">9833895800</a><br /><a href="mailto:info@tajpharma.com">info@tajpharma.com</a>
+            </td>
+            <td>Available</td>
+            <td>Rs 35,000 only, Payment via account Courier via Mumbai</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>14</td>
+            <td>India</td>
+            <td>Hetero Healthcare for COVIFOR Helpline number</td>
+            <td>Toll Free:<a href="tel:18001034696">18001034696</a>
+            </td>
+            <td>Currently Waiting List</td>
+            <td><b><u>Process:</u></b><br />Call on the toll free number and register yourself(Registration is
+              compulsory). Then
+              you will receive a link on your phone after registration. Fill in all the details and <b>before submitting
+                take a screenshot of the details filled in. Without this screenshot injection won't be given.</b>After
+              that send the below things to whatsapp number: <a href="tel:+919320985814">9320985814</a><br />1)
+              Screenshot
+              Taken<br />2)Original Prescription<br />3)Adhaar Card Photo<br />4)Covid +ve report.</td>
+            <td>21/04/2021</td>
+          </tr>
+          <tr>
+            <td>15</td>
+            <td>Rajkot</td>
+            <td>Rajkot Remdesivir Helpline</td>
+            <td>Send your case number and report details on Whatsapp</td>
+            <td>Available</td>
+            <td></td>
+            <td>21/04/2021</td>
+          </tr>
+          <tr>
+            <td>16</td>
+            <td>Ahmedabad</td>
+            <td>Hetero</td>
+            <td>Read notes</td>
+            <td>Available</td>
+            <td><a href="tel=+919768283810">9768283810</a></td>
+            <td>24/04/2021</td>
+          </tr>
+          <tr>
+            <td>17</td>
+            <td>Ahmedabad</td>
+            <td>Deep</td>
+            <td>Read notes - Remedesivir 100/Actimera 400</td>
+            <td>Available</td>
+            <td><a href="tel=+918097253459">8097253459</a><br /><a href="tel=+918999375256">8999375256</a></td>
+            <td>24/04/2021</td>
+          </tr>
+          <tr>
+            <td>18</td>
+            <td>Ahmedabad</td>
+            <td>Nexus</td>
+            <td>Read notes - Remedesivir 100/Actimera 400</td>
+            <td>Available</td>
+            <td><a href="tel=+919664400575">9664400575</a></td>
+            <td>24/04/2021</td>
+          </tr>
+          <tr>
+            <td>19</td>
+            <td>Ahmedabad</td>
+            <td>Royal</td>
+            <td>Read notes - Remedesivir 100/Actimera 400</td>
+            <td>Available</td>
+            <td><a href="tel=+919820344456">9820344456</a></td>
+            <td>24/04/2021</td>
+          </tr>
+        </tbody>
       </table>
 
     </div>
