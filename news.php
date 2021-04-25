7ca4b9ea4d9ca8434a2f3c67f5334e424f0b989b
Saransh <saranshs@nookal.com>
2021-04-25 18:22:02 +0530

    moment error

--- news.php
+++ news.php
@@ -45,7 +45,7 @@
 <script src="https://kit.fontawesome.com/852453daca.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
   integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.worker.min.js"></script>
 
 <script src="/assets/js/glider.min.js"></script>
