71bfeae803580a336c15cec94910940ffb1771e8
Saransh <saranshs@nookal.com>
2021-04-25 18:02:32 +0530

    SEO

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <!-- Fav Icon  -->
   <link rel="shortcut icon" href="images/favicon.svg">
   <!-- Site Title  -->
-  <title>COVID-19 Madad | Initiative to help COVID affected</title>
+  <title>COVID 19 Madad Gujarat | Initiative to help COVID affected</title>
   <!-- Bundle and Base CSS -->
   <link rel="stylesheet" href="assets/css/bundle.css">
   <link rel="stylesheet" href="assets/css/styles.css">
