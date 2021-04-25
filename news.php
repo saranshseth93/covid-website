146b4a531c31ab024786ec55aa30ef31954a5805
Saransh <saranshs@nookal.com>
2021-04-25 14:30:02 +0530

    Scroll to top

--- news.php
+++ news.php
@@ -30,6 +30,12 @@
 </div>
 
 
+<!-- Back to top button -->
+<button type="button" class="btn btn-danger btn-floating btn-lg" id="btn-back-to-top">
+  <i class="fas fa-arrow-up"></i>
+</button>
+
+
 
 
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
