6e74d7a50cb8cfdf45cc0598eab0dd03b0bb15a6
Saransh <saranshs@nookal.com>
2021-04-25 20:48:57 +0530

    SEO optmisation

--- index.php
+++ index.php
@@ -98,7 +98,7 @@
                       </div><!-- .col -->
 
                       <div class="col-lg-12 col-xl-12" data-covid="world" data-covid-update="last-update">
-                        <h1 class="status-title">Covid 19 Gujarat <span class="text-small">(Last 24 hours)</span></h1>
+                        <h1 class="status-title">Covid 19 Gujarat <span class="text-muted">(Last 24 hours)</span></h1>
                         <div class="row g-gs pr-4">
                           <div class="col-sm-4 col-6">
                             <div class="status-item">
