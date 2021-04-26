60380814187e33ed8edd553f9967f060f91387df
Saransh <saranshs@nookal.com>
2021-04-26 13:02:15 +0530

    Oxygen lead

--- assets/js/news.js
+++ assets/js/news.js
@@ -47,8 +47,6 @@ jQuery(
       $(".popup-gallery").magnificPopup({
         delegate: "a",
         type: "image",
-        tLoading: "Loading image #%curr%...",
-        mainClass: "mfp-img-mobile",
         gallery: {
           enabled: true,
           preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
