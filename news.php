5e897d0de9e421f0e5e04da8c70c14ad631a5d39
Saransh <saranshs@nookal.com>
2021-04-26 12:41:06 +0530

    Popup gallery

--- news.php
+++ news.php
@@ -1,9 +1,10 @@
 <link rel="stylesheet" href="assets/css/glider.min.css">
 <link rel="stylesheet" href="assets/css/news.css">
+<link rel="stylesheet" href="/assets/css/magnific-popup.css">
 
 <div class="row g-gs align-items-center justify-content-between">
   <div class="glider-contain">
-    <div class="glider">
+    <div class="glider popup-gallery">
       <?php 
           $directory = "news";
           $images = glob($directory . "/*.{jpg,png,jpeg,gif,PNG,JPG,JPEG,GIF}", GLOB_BRACE);
@@ -15,8 +16,10 @@
           foreach($images as $image)
           { ?>
       <div class="text-center">
-        <?php echo '<img style="max-height: 400px;" src="'.$image .'" alt="'.$image.'" />';
+        <a href="<?php echo $image; ?>">
+          <?php echo '<img style="max-height: 400px;" src="'.$image .'" alt="'.$image.'" />';
         ?>
+        </a>
       </div>
       <?php } ?>
     </div>
@@ -42,13 +45,15 @@
 
 
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script defer src="/assets/fontawesome/js/all.js"></script> <!--load all styles -->
+<script defer src="/assets/fontawesome/js/all.js"></script>
+<!--load all styles -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
   integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.worker.min.js"></script>
 
 <script src="/assets/js/glider.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
 <script src="assets/js/bundle.js"></script>
 <script src="assets/js/scripts.js"></script>
 <script src="assets/js/news.js"></script>
