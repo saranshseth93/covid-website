aaa2fef592ca18cc59c5a67e7bf8918670666288
Saransh <saranshs@nookal.com>
2021-04-26 20:01:17 +0530

    Patient bed update

---
+++ assets/js/vaccination-map.js
@@ -0,0 +1,437 @@
+var _0x4c65 = [
+  "https://apis.mapmyindia.com/advancedmaps/v1/",
+  "none",
+  "3RZGPhL",
+  "<p><a\x20href=\x22https://eloc.me/",
+  "token",
+  "Map",
+  "toFixed",
+  "https://maps.mapmyindia.com/cowin-app/icon/icon_Va_d.png",
+  "80438BUdrBn",
+  "<link\x20rel=\x22stylesheet\x22\x20href=\x22",
+  "http://10.10.21.117/map-production/MapApp_Web/map7.2/",
+  "mmiMap",
+  "<div\x20class=\x22mapSec\x22>",
+  "<div\x20class=\x22inputSec\x22>",
+  "string",
+  "1EdpXtc",
+  "onload",
+  "<center>Loading...</center>",
+  "NaN",
+  "350346jxRqyp",
+  "\x20mts\x20",
+  "<div\x20onclick=\x22if(NMarker)\x20NMarker.fire(\x27",
+  "register",
+  "script",
+  "remove",
+  "<input\x20class=\x22inputfield\x22\x20id=\x22",
+  "nearby",
+  "Current\x20Location",
+  "\x22\x20class=\x22blueBtn\x22\x20target=\x22blank\x22id=\x22register\x22\x20style=\x22display:none\x22>Register\x20Yourself</a>",
+  "click",
+  "addListener",
+  "15046LNyWss",
+  "placeAddress",
+  "219300jzdrvb",
+  "</div><style>#attr\x20span\x20b{display:none}.mmi_popup{width:250px}.mmi_popup\x20p{margin:10px\x205px\x20-6px\x2010px!important;}</style>",
+  "search",
+  "<div\x20class=\x22mainContainer\x22>",
+  "appendChild",
+  "split",
+  "<button\x20class=\x22seachBtn\x22\x20type=\x22submit\x22>Go</button>",
+  "nearby-vaccine-centres-mapmyindia",
+  "https://apis.mapmyindia.com/advancedmaps/api/",
+  "/map_sdk_plugins",
+  "<div\x20class=\x22mapContent\x22>",
+  "616835wCpRrH",
+  "href",
+  "Div\x20not\x20found\x20for\x20plugin!!",
+  "<li>No\x20data\x20available</li>",
+  "https://maps.mapmyindia.com/cowin-app/icon/icon_Va.png",
+  "Vaccination+Centre",
+  "onreadystatechange",
+  "1\x22\x20type=\x22text\x22\x20value=\x22\x22\x20placeholder=\x22Enter\x20place/address/eLoc\x22>",
+  "setAttribute",
+  "<div\x20\x20style=\x22position:\x20relative;\x20overflow:\x20visible;clear:both;padding-top:10px\x22><div\x20id=\x22mCSB_1\x22\x20class=\x22mCustomScrollBox\x20mCS-minimal-dark\x20mCSB_vertical\x20mCSB_outside\x22\x20style=\x22max-height:\x20none;\x22\x20tabindex=\x220\x22><div\x20id=\x22mCSB_1_container\x22\x20class=\x22mCSB_container\x22\x20style=\x22position:\x20relative;\x20top:\x200px;\x20left:\x200px;\x22\x20dir=\x22ltr\x22>",
+  "12498XsxRdu",
+  "YTEwZTBkZDJjZDcxMzg2ZWU0YWI5MDg3M2U4MDRlYjg=",
+  "category",
+  "3DSwZTV",
+  "ready",
+  "push",
+  "style",
+  "complete",
+  "placeName",
+  "innerHTML",
+  "10.10.21.117",
+  "location",
+  "length",
+  "random",
+  "\x20km\x20",
+  "<div\x20class=\x22mapDiv\x22><div\x20class=\x22map\x22\x20id=\x22",
+  "indexOf",
+  "MMI_near",
+  "/map_load?v=1.5&layer1=vector&callback=mapLoad",
+  "readyState",
+  "longitude",
+  "\x27)\x22\x20style=\x22cursor:pointer\x22><h3>",
+  "css",
+  "geo",
+  "latitude",
+  "head",
+  "</div></div><div\x20style=\x22display:\x20block;\x22><div\x20class=\x22mCSB_draggerContainer\x22><div\x20id=\x22mCSB_1_dragger_vertical\x22\x20class=\x22mCSB_dragger\x22\x20style=\x22position:\x20absolute;\x20min-height:\x2050px;\x20display:\x20block;\x20height:\x2085px;\x20max-height:\x20273px;\x20top:\x200px;\x22><div\x20class=\x22mCSB_dragger_bar\x22\x20style=\x22line-height:\x2050px;\x22></div></div><div\x20class=\x22mCSB_draggerRail\x22></div></div></div></div>",
+  "registerUrl",
+  "</span></a></p>",
+  "</h3><p>",
+  "https://maps.mapmyindia.com/",
+  "error",
+  "createElement",
+  "cowin-app/css/custom.css?",
+  "src",
+  "value",
+  "object",
+  "getElementById",
+  "display",
+  "text/javascript",
+  "eloc",
+  "block",
+  "type",
+  "current_location",
+  "eLoc",
+  "data",
+  "<div\x20class=\x22searchDiv\x22>",
+  "90983iWARdz",
+  "</div>"
+];
+var _0x519a = function (_0x10ec87, _0x1d0ba5) {
+  _0x10ec87 = _0x10ec87 - 0x165;
+  var _0x4c6500 = _0x4c65[_0x10ec87];
+  return _0x4c6500;
+};
+var _0x41b343 = _0x519a;
+(function (_0x3b8248, _0x508f64) {
+  var _0x2f229f = _0x519a;
+  while (!![]) {
+    try {
+      var _0x6d6897 =
+        parseInt(_0x2f229f(0x180)) * -parseInt(_0x2f229f(0x1a7)) +
+        parseInt(_0x2f229f(0x19d)) +
+        parseInt(_0x2f229f(0x192)) +
+        -parseInt(_0x2f229f(0x16f)) * parseInt(_0x2f229f(0x173)) +
+        -parseInt(_0x2f229f(0x184)) +
+        parseInt(_0x2f229f(0x1aa)) * parseInt(_0x2f229f(0x190)) +
+        parseInt(_0x2f229f(0x179));
+      if (_0x6d6897 === _0x508f64) break;
+      else _0x3b8248["push"](_0x3b8248["shift"]());
+    } catch (_0x422a78) {
+      _0x3b8248["push"](_0x3b8248["shift"]());
+    }
+  }
+})(_0x4c65, 0x4f91e);
+var Mcat = _0x41b343(0x1a2),
+  MMInearId = "",
+  NMarker = "",
+  NMarker_focus = "",
+  defcnter = [28.09, 78.3],
+  mcnter = [28.09, 78.3],
+  Mcss =
+    (window[_0x41b343(0x1b2)][_0x41b343(0x19e)][_0x41b343(0x1b7)](
+      _0x41b343(0x1b1)
+    ) == -0x1
+      ? _0x41b343(0x1c5)
+      : _0x41b343(0x17b)) +
+    _0x41b343(0x1c8) +
+    Math["floor"](Math[_0x41b343(0x1b4)]() * 0x3e8),
+  searchId = "",
+  mapObj = [],
+  mapId = _0x41b343(0x17c),
+  Mapno = 0x1,
+  MMIjw = _0x41b343(0x1a8),
+  registerUrl = "https://selfregistration.cowin.gov.in";
+function mapLoad(_0x1d74c3) {
+  var _0x250f6b = _0x41b343;
+  mapObj[mapId] = new MapmyIndia[_0x250f6b(0x176)](
+    document[_0x250f6b(0x165)](mapId),
+    {
+      center: "string" === typeof mcnter ? defcnter : mcnter,
+      zoom: 0x4,
+      fullscreenControl: ![],
+      fullscreen: ![],
+      scrollwheel: !![],
+      layers: ![],
+      search: ![]
+    }
+  );
+  var _0x19a507 = document[_0x250f6b(0x1c7)](_0x250f6b(0x188));
+  _0x19a507["setAttribute"](
+    _0x250f6b(0x1c9),
+    _0x250f6b(0x19a) + atob(MMIjw) + _0x250f6b(0x19b)
+  ),
+    _0x19a507[_0x250f6b(0x1a5)](_0x250f6b(0x16a), _0x250f6b(0x167)),
+    document[_0x250f6b(0x1c0)][_0x250f6b(0x196)](_0x19a507),
+    (_0x19a507[_0x250f6b(0x181)] = function () {
+      var _0x2d96f7 = _0x250f6b;
+      MapmyIndia[_0x2d96f7(0x194)](
+        document["getElementById"](searchId),
+        {
+          tokenizeAddress: ![],
+          bridge: ![]
+        },
+        function (_0x487507) {
+          var _0x5423b5 = _0x2d96f7;
+          MMIcat(searchId, Mcat, _0x487507[0x0], mapObj[mapId]),
+            _0x487507[0x0] &&
+              (document[_0x5423b5(0x165)](searchId)["value"] =
+                _0x487507[0x0][_0x5423b5(0x1af)]);
+        }
+      ),
+        MapmyIndia[_0x2d96f7(0x194)](
+          document[_0x2d96f7(0x165)](searchId + "1"),
+          {
+            tokenizeAddress: ![],
+            bridge: ![]
+          },
+          function (_0x3c8e58) {
+            MMIcat(searchId + "1", Mcat, _0x3c8e58[0x0], mapObj[mapId]);
+          }
+        ),
+        parseFloat(mcnter[0x0]) != 28.09 &&
+          parseFloat(mcnter[0x0]) != 78.3 &&
+          setTimeout(function () {
+            var _0x2f4ca3 = _0x2d96f7;
+            if (document[_0x2f4ca3(0x165)](searchId))
+              document[_0x2f4ca3(0x165)](searchId)["value"] = mcnter;
+            if (document[_0x2f4ca3(0x165)](searchId + "1"))
+              document[_0x2f4ca3(0x165)](searchId + "1")[
+                _0x2f4ca3(0x1ca)
+              ] = mcnter;
+            MMIcat(
+              searchId,
+              Mcat,
+              {
+                eLoc: mcnter,
+                latitude: parseFloat(mcnter[0x0]),
+                longitude: parseFloat(mcnter[0x1])
+              },
+              mapObj[mapId]
+            );
+          }, 0x5);
+    });
+}
+function MMIcat(_0x56615f, _0x58829c, _0x26bb64, _0x6cd276) {
+  var _0x1c0ea5 = _0x41b343,
+    _0x55f504 = _0x26bb64[_0x1c0ea5(0x16c)],
+    _0x10b9f1 = _0x26bb64[_0x1c0ea5(0x1bf)],
+    _0x5c13a8 = _0x26bb64[_0x1c0ea5(0x1bb)];
+  if (_0x10b9f1 && _0x10b9f1 != _0x1c0ea5(0x183))
+    _0x55f504 = _0x10b9f1 + "," + _0x5c13a8;
+  if (_0x58829c && _0x55f504) {
+    if (NMarker) NMarker[_0x1c0ea5(0x189)]();
+    if (NMarker_focus) NMarker_focus[_0x1c0ea5(0x189)]();
+    (document[_0x1c0ea5(0x165)](MMInearId)["innerHTML"] = _0x1c0ea5(0x182)),
+      setTimeout(function () {
+        var _0x5e4c10 = _0x1c0ea5,
+          _0x58dfce = MapmyIndia[_0x5e4c10(0x18b)]({
+            keywords: encodeURI(_0x58829c),
+            refLocation: _0x55f504
+          });
+        (document[_0x5e4c10(0x165)](MMInearId)[_0x5e4c10(0x1b0)] = ""),
+          show_category(_0x56615f, _0x58dfce, _0x6cd276);
+      }, 0xa);
+  }
+}
+function show_category(_0x1b0951, _0x4ca7bc, _0x10006c) {
+  var _0x24a010 = _0x41b343,
+    _0x272919 = _0x4ca7bc[_0x24a010(0x16d)];
+  if (_0x272919) {
+    var _0x468ad1 = "",
+      _0x593837 = [],
+      _0x10d3df = [],
+      _0x1b434d = document[_0x24a010(0x165)](_0x1b0951);
+    for (
+      var _0x38dd9a = 0x0;
+      _0x38dd9a < _0x272919[_0x24a010(0x1b3)];
+      _0x38dd9a++
+    ) {
+      var _0xec3df6 = _0x272919[_0x38dd9a],
+        _0x741be1 = _0xec3df6["placeName"],
+        _0x39ac4f = _0xec3df6[_0x24a010(0x16c)],
+        _0x5022da = _0xec3df6["distance"],
+        _0x4a3787 = _0xec3df6[_0x24a010(0x191)],
+        _0x2dc96f = "";
+      if (_0x1b434d && _0x1b434d["value"] == _0x24a010(0x18c)) {
+        if (
+          MapmyIndia["current_location"] &&
+          !MapmyIndia[_0x24a010(0x16b)][0x0]
+        )
+          MapmyIndia[_0x24a010(0x1be)](_0x10006c, 0x0);
+        _0x2dc96f =
+          _0x5022da >= 0x3e8
+            ? (_0x5022da / 0x3e8)["toFixed"](0x2) + _0x24a010(0x1b5)
+            : _0x5022da[_0x24a010(0x177)](0x2) + _0x24a010(0x185);
+      }
+      var _0x3e3bea = _0x39ac4f;
+      _0xec3df6["latitude"] &&
+        (_0x3e3bea =
+          _0xec3df6[_0x24a010(0x1bf)] + "," + _0xec3df6["longitude"]);
+      var _0x356258 =
+        _0x24a010(0x186) +
+        _0x3e3bea +
+        _0x24a010(0x1bc) +
+        _0x741be1 +
+        _0x24a010(0x1c4) +
+        _0x4a3787 +
+        "</p></div>" +
+        _0x24a010(0x174) +
+        _0x39ac4f +
+        "\x22\x20target=\x22blank\x22\x20style=\x22color:#2275d7\x22><b>Get\x20Direction</b><br/><span>" +
+        _0x2dc96f +
+        _0x24a010(0x1c3);
+      (_0x468ad1 += "<li>" + _0x356258 + "</li>"),
+        _0x593837[_0x24a010(0x1ac)](_0x3e3bea),
+        _0x10d3df[_0x24a010(0x1ac)](_0x356258);
+    }
+    document[_0x24a010(0x165)](MMInearId)[_0x24a010(0x1b0)] = _0x468ad1;
+    if (_0x468ad1)
+      document[_0x24a010(0x165)](_0x24a010(0x187))[_0x24a010(0x1ad)][
+        _0x24a010(0x166)
+      ] = _0x24a010(0x169);
+    else
+      _0x468ad1 &&
+        (document["getElementById"](_0x24a010(0x187))["style"][
+          "display"
+        ] = _0x24a010(0x172));
+    if (NMarker) NMarker[_0x24a010(0x189)]();
+    var _0x2f1e6d = {
+      url: _0x24a010(0x178),
+      width: 0x1e,
+      height: 0x28,
+      popupAnchor: [0x0, -0x1e]
+    };
+    (NMarker = MapmyIndia["elocMarker"]({
+      map: _0x10006c,
+      eloc: _0x593837,
+      icon: _0x2f1e6d,
+      popupHtml: _0x10d3df,
+      offset: [0xf, 0x28]
+    })),
+      NMarker["fitbounds"](),
+      NMarker[_0x24a010(0x18f)](_0x24a010(0x18e), function (_0x23a784) {
+        var _0x3ef6d5 = _0x24a010;
+        if (_0x23a784) {
+          var _0x25a004 = _0x23a784[_0x3ef6d5(0x168)];
+          if (NMarker_focus) NMarker_focus[_0x3ef6d5(0x189)]();
+          var _0x54e2ff = {
+            url: _0x3ef6d5(0x1a1),
+            width: 0x1e,
+            height: 0x28
+          };
+          NMarker_focus = MapmyIndia["elocMarker"]({
+            map: _0x10006c,
+            eloc: _0x25a004,
+            icon: _0x54e2ff,
+            offset: [0xf, 0x28]
+          });
+        }
+      }),
+      NMarker[_0x24a010(0x18f)]("popupclose", function () {
+        var _0x3990e5 = _0x24a010;
+        if (NMarker_focus) NMarker_focus[_0x3990e5(0x189)]();
+      });
+  } else {
+    var _0x2559ff = document[_0x24a010(0x165)](MMInearId);
+    if (_0x2559ff) _0x2559ff["innerHTML"] = _0x24a010(0x1a0);
+  }
+}
+function loadNearbyVaccineCentresMapmyIndiaDiv(_0x188583, _0x2df7b9) {
+  var _0x44d281 = _0x41b343;
+  mapId += Mapno++;
+  if (_0x2df7b9 && _0x2df7b9[_0x44d281(0x1a9)])
+    Mcat = _0x2df7b9[_0x44d281(0x1a9)];
+  if (_0x2df7b9 && _0x2df7b9["registerUrl"])
+    registerUrl = _0x2df7b9[_0x44d281(0x1c2)];
+  if (_0x2df7b9 && _0x2df7b9["css"]) Mcss = _0x2df7b9[_0x44d281(0x1bd)];
+  if (_0x2df7b9 && _0x2df7b9["token"])
+    MMIjw = btoa(_0x2df7b9[_0x44d281(0x175)]);
+  _0x44d281(0x17f) == typeof _0x188583 &&
+    (_0x188583 = document[_0x44d281(0x165)](_0x188583));
+  if (!_0x188583)
+    return document[_0x44d281(0x1ba)] === _0x44d281(0x1ab) ||
+      document[_0x44d281(0x1ba)] === _0x44d281(0x1ae)
+      ? (alert(_0x44d281(0x19f)), ![])
+      : ((document[_0x44d281(0x1a3)] = function () {
+          var _0x400a24 = _0x44d281;
+          if (document["readyState"] == _0x400a24(0x1ae)) {
+            var _0x479278 = document[_0x400a24(0x165)](_0x400a24(0x199));
+            if (_0x479278)
+              loadNearbyVaccineCentresMapmyIndiaDiv(_0x479278, _0x2df7b9);
+            else console[_0x400a24(0x1c6)](_0x400a24(0x19f));
+            return ![];
+          }
+        }),
+        ![]);
+  if (_0x44d281(0x1cb) !== typeof MapmyIndia) {
+    var _0xf8707 = document[_0x44d281(0x1c7)](_0x44d281(0x188));
+    if (_0x2df7b9[_0x44d281(0x1b2)]) {
+      if (_0x44d281(0x1cb) == typeof _0x2df7b9[_0x44d281(0x1b2)])
+        _0x2df7b9[_0x44d281(0x1b2)] = _0x2df7b9[_0x44d281(0x1b2)]["toString"]();
+      var _0x2c6687 = _0x2df7b9[_0x44d281(0x1b2)][_0x44d281(0x197)](",");
+      if (
+        (_0x2c6687 &&
+          parseFloat(_0x2c6687[0x0]) &&
+          parseFloat(_0x2c6687[0x1])) ||
+        _0x2df7b9[_0x44d281(0x1b2)][_0x44d281(0x1b3)] == 0x6
+      ) {
+        if (_0x2c6687[0x1])
+          mcnter = [parseFloat(_0x2c6687[0x0]), parseFloat(_0x2c6687[0x1])];
+        else mcnter = _0x2c6687[0x0];
+      }
+    }
+    _0xf8707[_0x44d281(0x1a5)](
+      _0x44d281(0x1c9),
+      _0x44d281(0x171) + atob(MMIjw) + _0x44d281(0x1b9)
+    ),
+      _0xf8707[_0x44d281(0x1a5)](_0x44d281(0x16a), _0x44d281(0x167)),
+      document[_0x44d281(0x1c0)]["appendChild"](_0xf8707);
+  } else mapLoad();
+  (searchId = mapId + "_search"), (MMInearId = _0x44d281(0x1b8));
+  var _0x286efe =
+    _0x44d281(0x17a) +
+    Mcss +
+    "\x22>" +
+    _0x44d281(0x195) +
+    _0x44d281(0x17d) +
+    "<div\x20class=\x22searchDiv\x22>" +
+    _0x44d281(0x17e) +
+    _0x44d281(0x18a) +
+    searchId +
+    "\x22\x20type=\x22text\x22\x20value=\x22\x22\x20placeholder=\x22Enter\x20place/address/eLoc\x22>" +
+    _0x44d281(0x198) +
+    _0x44d281(0x170) +
+    "</div>" +
+    _0x44d281(0x1b6) +
+    mapId +
+    "\x22></div></div>" +
+    "</div>" +
+    _0x44d281(0x19c) +
+    _0x44d281(0x16e) +
+    _0x44d281(0x17e) +
+    _0x44d281(0x18a) +
+    searchId +
+    _0x44d281(0x1a4) +
+    _0x44d281(0x198) +
+    _0x44d281(0x170) +
+    _0x44d281(0x170) +
+    _0x44d281(0x1a6) +
+    "<ul\x20class=\x22searchList\x22\x20id=\x22" +
+    MMInearId +
+    "\x22>" +
+    "</ul>" +
+    "<a\x20href=\x22" +
+    registerUrl +
+    _0x44d281(0x18d) +
+    _0x44d281(0x1c1) +
+    _0x44d281(0x170) +
+    _0x44d281(0x193);
+  _0x188583[_0x44d281(0x1b0)] = _0x286efe;
+}
--- assets/js/vaccination.js
+++ assets/js/vaccination.js
@@ -1,15 +1,1 @@
-$(document).ready(function () {
-  function alert(message) {
-    console.log(message);
-  }
-
-  setTimeout(() => {
-    loadNearbyVaccineCentresMapmyIndiaDiv(
-      document.getElementById("nearby-vaccine-centres-mapmyindia"),
-      {
-        css: "https://prod-cdn.preprod.co-vin.in/assets/css/map-custom.css",
-        registerUrl: "https://selfregistration.cowin.gov.in/"
-      }
-    );
-  }, 5000);
-});
+$(document).ready(function () {});
