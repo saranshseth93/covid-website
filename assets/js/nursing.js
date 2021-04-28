2dd0beebad5b74b285ad197e516f2c4f9ae22e0f
Saransh <saranshs@nookal.com>
2021-04-28 19:40:25 +0530

    Nursing Staff

---
+++ assets/js/nursing.js
@@ -0,0 +1,47 @@
+jQuery(
+  (function () {
+    "use strict";
+
+    $.getJSON(
+      "https://content-sheets.googleapis.com/v4/spreadsheets/1ZyrYsowjk6PdC9N5yKBxMslI7FypoeIqDvlAYrqprL8/values/Nursing%20Staff!A3%3AI?key=AIzaSyCAp-t_WKWx0sfpOM5_-5yeC9g-ZcrkWcI",
+      function (data) {
+        console.log(data);
+        if (
+          data != undefined &&
+          data.values != undefined &&
+          data.values.length != 0
+        ) {
+          let html = "";
+          $.each(data.values, function (i, v) {
+            let date = moment(v[8], "DD/MM/YYYY HH:mm:ss");
+            let now = moment();
+            let formattedDate = now.from(date, "days") + " ago";
+
+            html += `<tr><td>${v[0]}</td><td>${v[1]}</td><td>${v[2]}</td><td>${
+              v[3]
+            }</td><td>${v[4]}</td><td>${
+              v[5] != undefined ? v[5] : ""
+            }</td><td>${v[6] != undefined ? v[6] : ""}</td><td data-order=${
+              v[8] != "" ? moment(v[8], "DD/MM/YYYY HH:mm:ss").format("X") : ""
+            }>${
+              formattedDate == "Invalid date ago"
+                ? v[8] == undefined
+                  ? ""
+                  : v[8]
+                : formattedDate
+            }</td></tr>`;
+          });
+
+          $("#nursing-table").html(html);
+        }
+      }
+    ).then(function () {
+      $(".table").DataTable({
+        paging: false,
+        info: false,
+        aaSorting: [[7, "desc"]],
+        fixedHeader: true
+      });
+    });
+  })()
+);
---
+++ nursing.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="Saransh Seth">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="An Initiative to help COVID Affected">
+  <!-- Fav Icon  -->
+  <link rel="shortcut icon" href="images/favicon.svg">
+  <!-- Site Title  -->
+  <title>COVID-19 Madad | Nursing Staff</title>
+  <!-- Bundle and Base CSS -->
+  <link href="/assets/fontawesome/css/all.css" rel="stylesheet">
+  <!--load all styles -->
+  <link rel="stylesheet" href="assets/css/bundle.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.1.8/css/fixedHeader.dataTables.min.css" />
+  <link rel="stylesheet" href="assets/css/remedesivir.css">
+  <?php include(google-tag.html); ?>
+</head>
+
+<body class="nk-body">
+  <div class="top-bar">COVID 19 Helpline <a href="tel:+917926403333">079 26403333</a> | <a href="tel:+917926409999">079
+      26409999</a></div>
+  <?php include('header-2.html'); ?>
+  <div class="nk-banner">
+    <div class="container-lg">
+      <div class="row">
+        <h2 class="col">Nursing Staff</h2>
+      </div>
+
+      <table class="table table-striped table-bordered table-responsive-md mt-3">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>City</th>
+            <th>Organisation</th>
+            <th>Contact Person</th>
+            <th>Contact</th>
+            <th>Status</th>
+            <th>Remarks</th>
+            <th>Verified on</th>
+          </tr>
+        </thead>
+        <tbody id="nursing-table"></tbody>
+
+      </table>
+
+    </div>
+  </div>
+
+  <main class="nk-pages">
+    <section class="section section-l bg-light section-spread" id="news">
+      <div class="container">
+        <div class="section-content">
+          <div class="row g-gs justify-content-between">
+            <div class="col-lg-auto">
+              <div class="text-block text-center mb-4">
+                <h5 class="subtitle">Latest</h5>
+                <h2 class="title">News & Updates</h2>
+                <h6 class="subtitle">*Sharing as received*</h6>
+              </div>
+              <?php include('news.php'); ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <script defer src="/assets/fontawesome/js/all.js"></script>
+  <!--load all styles -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/fixedheader/3.1.8/js/dataTables.fixedHeader.min.js"></script>
+  <script src="/assets/js/nursing.js"></script>
+  <?php include('footer.html'); ?>
+</body>
+
+</html>
