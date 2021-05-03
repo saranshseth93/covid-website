45b0827cc7eac0383c4a569eeb0e1034a7dbce32
Saransh <saranshs@nookal.com>
2021-05-03 21:58:25 +0530

    Doctor on Call update

--- assets/js/doctor.js
+++ assets/js/doctor.js
@@ -2,6 +2,7 @@ jQuery(
   (function () {
     "use strict";
 
+    let html = "";
     $.getJSON(
       "https://content-sheets.googleapis.com/v4/spreadsheets/1ZyrYsowjk6PdC9N5yKBxMslI7FypoeIqDvlAYrqprL8/values/Doctor%20on%20Call!A3%3AH?key=AIzaSyCAp-t_WKWx0sfpOM5_-5yeC9g-ZcrkWcI",
       function (data) {
@@ -11,7 +12,6 @@ jQuery(
           data.values != undefined &&
           data.values.length != 0
         ) {
-          let html = "";
           $.each(data.values, function (i, v) {
             let date = moment(v[7], "DD/MM/YYYY HH:mm:ss");
             let now = moment();
@@ -19,19 +19,43 @@ jQuery(
 
             html += `<tr><th>${v[0]}</th><td>${v[1]}</td><td>${v[2]}</td><td>${v[3]}</td><td>${v[4]}</td><td>${v[5]}</td><td>${formattedDate}</td></tr>`;
           });
-
-          $("#doctor-table").html(html);
         }
       }
     ).then(function () {
-      $(".table").DataTable({
-        paging: false,
-        info: false,
-        aaSorting: [[6, "asc"]],
-        fixedHeader: true
-      });
+      $.getJSON(
+        "https://content-sheets.googleapis.com/v4/spreadsheets/1TWR-pn9606h-xJ2sTr9wpkn84STsJh7xUWskONlIoNs/values/Doctor!A2%3AH43?key=AIzaSyBveiHShw1LaPLCWYiuSPW_fzIb7Gtan68",
+        function (response) {
+          if (
+            response != undefined &&
+            response.values != undefined &&
+            response.values.length != 0
+          ) {
+            let html2 = "";
+            $.each(response.values, function (i, v) {
+              let date = moment(v[6], "DD/MM/YYYY HH:mm:ss");
+              let now = moment();
+              let formattedDate = now.from(date, "days") + " ago";
 
-      $('input[aria-controls="DataTables_Table_0"]').addClass("form-control");
+              html2 += `<tr><th>${v[0]}</th><td>${v[1]}</td><td>${
+                v[2]
+              }</td><td>${v[3]}</td><td>${v[4]}</td><td>${v[5]}</td><td>${
+                formattedDate != "Invalid date ago" ? formattedDate : v[6]
+              }</td></tr>`;
+            });
+
+            $("#doctor-table").html(html + html2);
+          }
+        }
+      ).then(function () {
+        $(".table").DataTable({
+          paging: false,
+          info: false,
+          aaSorting: [[6, "desc"]],
+          fixedHeader: true
+        });
+
+        $('input[aria-controls="DataTables_Table_0"]').addClass("form-control");
+      });
     });
   })()
 );
--- doctor.php
+++ doctor.php
@@ -6,8 +6,11 @@
   <meta name="author" content="Saransh Seth">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="An Initiative to help COVID Affected">
-  <meta property="og:description" content="A volunteer-driven crowd sourced effort to track available hospital beds, plasma, food, ambulance and other critical services for Corona in Ahmedabad region." data-rh="true">
-  <meta name="keywords" content="hospital, beds, vacancy, ventilator, gujarat, ahmedabad, oxygen, concentrator, icu, covid, covid19, covid-19, coronavirus, corona, india, virus, pandemic, disease, carona, karona, korona, food, gandhinagar, amreli, tiffin, food, home care, covid care, plasma, vaccination, rtpcr, remedesivir, nursing, ambulance">
+  <meta property="og:description"
+    content="A volunteer-driven crowd sourced effort to track available hospital beds, plasma, food, ambulance and other critical services for Corona in Ahmedabad region."
+    data-rh="true">
+  <meta name="keywords"
+    content="hospital, beds, vacancy, ventilator, gujarat, ahmedabad, oxygen, concentrator, icu, covid, covid19, covid-19, coronavirus, corona, india, virus, pandemic, disease, carona, karona, korona, food, gandhinagar, amreli, tiffin, food, home care, covid care, plasma, vaccination, rtpcr, remedesivir, nursing, ambulance">
   <!-- Fav Icon  -->
   <link rel="shortcut icon" href="images/favicon.svg">
   <!-- Site Title  -->
@@ -15,7 +18,9 @@
   <!-- Bundle and Base CSS -->
   <link href="/assets/fontawesome/css/all.css" rel="stylesheet">
   <!--load all styles -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf_viewer.min.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.1.8/css/fixedHeader.dataTables.min.css" />
+
   <link rel="stylesheet" href="assets/css/bundle.css">
   <link rel="stylesheet" href="assets/css/styles.css">
   <link rel="stylesheet" href="assets/css/doctor.css">
@@ -86,7 +91,8 @@
   <!--load all styles -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"
     integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.worker.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/fixedheader/3.1.8/js/dataTables.fixedHeader.min.js"></script>
   <script src="assets/js/doctor.js"></script>
 
   <?php include('footer.html'); ?>
